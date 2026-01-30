519198dd65e5feaf25c60d4986feb26a0b702cd9
Musheer@8951 <musheer.khan@laitusneo.com>
2026-01-30 11:12:15 +0530

    updated

--- src/main/java/com/mippay/config/SecurityConfig.java
+++ src/main/java/com/mippay/config/SecurityConfig.java
@@ -58,8 +58,8 @@ public class SecurityConfig {
             "http://localhost:5174",
             "http://65.1.113.7:8083",
             "http://65.1.113.7:8084",
-            "https://admin.mippay.in",
-            "https://merchant.mippay.in"
+            "admin.mippay.meihitech.com",
+            "merchant.mippay.meihitech.com"
 
         ));
 
