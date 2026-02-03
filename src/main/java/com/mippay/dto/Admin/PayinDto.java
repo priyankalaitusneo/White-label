2f39cb15ab83ec87e70d8e8676a65995b64942d8
Priyanka Singh <priyanka.singh@laitusneo.com>
2026-02-03 12:50:54 +0530

    changes for logic and amount

--- src/main/java/com/mippay/dto/Admin/PayinDto.java
+++ src/main/java/com/mippay/dto/Admin/PayinDto.java
@@ -1,5 +1,9 @@
 package com.mippay.dto.Admin;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -17,7 +21,14 @@ public class PayinDto {
     private String accountNo;
     private String mobile;
     private String address;
-    private String amount;       // You can convert to BigDecimal if needed
+    
+    @NotBlank(message = "amount should not be null or empty")
+    @Pattern(
+        regexp = "^[1-9][0-9]{2,}$",
+        message = "amount must be at least 100 paisa"
+    )
+    private String amount;
+      // You can convert to BigDecimal if needed
     private String userId;
     private String redirectRoute;     
 }
